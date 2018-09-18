2e963fd178a85e5ffaffc27728366d5947a951e4
mkkemp94 <mkkemp94@gmail.com>
2018-09-17 21:57:57 -0400

    Finished score master (before refactoring)

--- Bowlmaster/Assets/Scripts/ScoreMaster.cs
+++ Bowlmaster/Assets/Scripts/ScoreMaster.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic; // for lists
 using UnityEngine;
 
@@ -13,15 +14,27 @@ public class ScoreMaster {
     {
         List<int> cumulativeScores = new List<int>();
         int runningTotal = 0;
-        foreach (int frameScore in ScoreFrames(rolls))
+
+        List<int> frames = ScoreFrames(rolls);
+        for (int i = 1; i <= frames.Count; i++)
         {
-            Debug.Log("This frame: " + frameScore);
+            int d = frames[i-1];
+            String s = String.Format("Frame {0} : {1}", i, d);
+            Debug.Log(s);
+        }
+        foreach (int frameScore in frames)
+        {
+            String s = String.Format("Running total == {0} + {1} == {2}", runningTotal, frameScore, (runningTotal + frameScore));
+            Debug.Log(s);
             runningTotal += frameScore;
-            Debug.Log("Running Total: " + frameScore);
+            
             cumulativeScores.Add(runningTotal);
         }
         return cumulativeScores;
     }
+
+    //private static bool spareActive = false;
+    //private static int frameNumber = 1;
 
     /**
      * Returns what can currently be calculated for frames.
@@ -33,29 +46,136 @@ public class ScoreMaster {
         int currentBowl = 1;
         int frameScore = 0;
 
+        // For debugging
+        int frameNumber = 1;
+        int rollNumber = 1;
+        bool spareActive = false;
+        bool strikeActive = false;
+        bool gameOver = false;
+
+        int activeStrikes = 0;
+
         // My code here
-        foreach (int roll in rolls)
+        //foreach (int roll in rolls)
+        for (int i = 0; i < rolls.Count; i++)
         {
-            Debug.Log("ScoreFrames: Roll score = " + roll);
-            frameScore += roll;
+            if (gameOver) break;
 
-            if (OddTurn(currentBowl) && roll == 10)
+            Debug.Log("ScoreFrames: Bowl " + currentBowl + " score = " + rolls[i]);
+
+            
+            frameScore += rolls[i];
+
+            // Fulfilling a spare with a strike. Add 10 to the spare and continue to add up for this turn.
+            if (rolls[i] == 10 && spareActive)
             {
-                currentBowl++;
-            }
-
-            if (EvenTurn(currentBowl))
-            {
-                Debug.Log("ScoreFrames: Frame Score = " + (frameScore));
-
+                Debug.Log("ScoreFrames: Frame " + frameNumber + " Score = " + (frameScore));
+                Debug.Log("ScoreFrames: Bowl " + currentBowl + " score = " + rolls[i]);
                 frameList.Add(frameScore);
+                //frameScore -= 10;
+                frameScore = rolls[i];
+                //Debug.Log("Pending frame score now " + frameScore);
+                frameNumber++;
+                spareActive = false;
+            }
+            // Got a strike
+            if (rolls[i] == 10 && rolls.Count >= 3 && currentBowl <= 19)
+            {
+                int score = 10 + rolls[i + 1] + rolls[i + 2];
+
+                Debug.Log(String.Format("Strike on frame {0}. Score == 10 + {1} + {2} == {3}", frameNumber, rolls[i + 1], rolls[i + 2], score));
+                //Debug.Log("ScoreFrames: Strike Frame " + frameNumber + " Score = " + score);
+                frameList.Add(score);
+                frameNumber++;
+
+                if (currentBowl < 19)
+                    currentBowl += 2;
+                else
+                    gameOver = true;
+
                 frameScore = 0;
             }
 
+            else if (OddTurn(currentBowl))
+            {
+                // First handle spare case
+                if (spareActive)
+                {
+                    Debug.Log("ScoreFrames: Frame " + frameNumber + " Score = " + (frameScore));
+                    Debug.Log("ScoreFrames: Bowl " + currentBowl + " score = " + rolls[i]);
+                    frameList.Add(frameScore);
+                    //frameScore -= 10;
+                    frameScore = rolls[i];
+                    //Debug.Log("Pending frame score now " + frameScore);
+                    frameNumber++;
+                    spareActive = false;
+                }
 
-            currentBowl++;
+                //// Skip second half of frame
+                if (rolls[i] == 10)
+                {
+                    //Debug.Log("ScoreFrames: Frame " + frameNumber + " Score = " + (frameScore));
+                    //frameList.Add(10 + rolls[i + 1] + rolls[i + 2]);
+                    currentBowl++;
+                    currentBowl++;
+                    //strikeActive = true;
+                    activeStrikes++;
+                }
+
+                // Proceed as normal
+                else
+                {
+                    //Debug.Log("Not strike");
+                    currentBowl++;
+                }
+                
+                //if (i == 20) 
+            }
+
+            else if (EvenTurn(currentBowl))
+            {
+                if (activeStrikes > 0)
+                {
+                    Debug.Log("ScoreFrames: Frame " + frameNumber + " Score = " + (frameScore));
+                    Debug.Log("ScoreFrames: Bowl " + currentBowl + " score = " + rolls[i]);
+                    frameList.Add(frameScore);
+                    frameScore -= 10;
+                    frameNumber++;
+                    //strikeActive = false;
+                    activeStrikes--;
+                }
+
+                // Got a spare
+                if (frameScore == 10)
+                {
+                    Debug.Log("ScoreFrames: Spare Active");
+                    spareActive = true;
+                }
+
+                // Proceed as normal
+                else
+                {
+                    Debug.Log("ScoreFrames: Frame " + frameNumber + " Score = " + (frameScore));
+
+                    frameList.Add(frameScore);
+
+                    // Reset frame score.
+                    frameScore = 0;
+                    frameNumber++;
+
+                }
+
+                // Always go to next bowl here.
+                currentBowl++;
+            }
+            rollNumber++;
         }
 
+
+        //Debug.Log("Frame list: ");
+        //foreach (int i in frameList) {
+        //    Debug.Log(i);
+        //}
         return frameList;
     }
 
