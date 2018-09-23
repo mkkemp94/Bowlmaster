3cc3444b102a530a81b3d616528ae66f583a3fb3
mkkemp94 <mkkemp94@gmail.com>
2018-09-23 14:34:01 -0400

    Wiring up scoreboard

---
+++ Bowlmaster/Assets/Scripts/ScoreDisplay.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreDisplay : MonoBehaviour {
+
+    public Text[] rollTexts, frameTexts;
+
+	// Use this for initialization
+	void Start () {
+		for (int i = 1; i <= rollTexts.Length; i++)
+        {
+            rollTexts[i - 1].text = "1"; ;
+        }
+
+        for (int i = 1; i <= frameTexts.Length; i++)
+        {
+            frameTexts[i - 1].text = i + ""; ;
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
